739bca4e9bac4ea17800d12790a5dc17abff1846
Muhammed Ali CH <muhammedcihan205@gmail.com>
2025-03-07 15:16:37 +0300

    visualCompleted

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const yukariButon = document.getElementById("yukariCik");
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('deneme');
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 300) {
-            yukariButon.style.display = "block"; // Kaydırınca buton görünsün
-        } else {
-            yukariButon.style.display = "none"; // Yukarı çıkınca buton gizlensin
-        }
-    });
+    let indexElements = document.querySelectorAll('.serviceContentİndex');
+    let detailBoxElements = document.querySelectorAll('.servicesDetailBox');
+    let detailArrowElements = document.querySelectorAll('.servicesDetailArrow');
 
-    yukariButon.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: "smooth" }); // Yumuşak kaydırma
+    detailBoxElements.forEach((detailBox, i) => {
+        detailBox.addEventListener('click', () => {
+
+            indexElements.forEach((index, j) => {
+                if (i !== j) { // Eğer şu an tıklanan öğe değilse
+                    index.classList.remove("listed");
+                    detailArrowElements[j].classList.remove("listedArrow");
+                }
+            });
+
+            // Şu an tıklanan öğe için açma/kapama işlemi
+            let index = indexElements[i];
+            let detailArrow = detailArrowElements[i];
+
+            if(index.classList.contains("listed")) {
+                index.classList.remove("listed");
+                detailArrow.classList.remove("listedArrow");
+            } 
+            else{
+                index.classList.add("listed");
+                detailArrow.classList.add("listedArrow");
+            }
+        });
     });
 });
