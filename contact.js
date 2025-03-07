7dd2d5c2785653499902bdf610711d331ef7d452
Muhammed Ali CH <muhammedcihan205@gmail.com>
2025-03-07 17:19:09 +0300

    mailproblem

--- contact.js
+++ contact.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded',()=>{
+
     console.log("contact.js activated")
-});
+
+    function sendMail(){
+        let informations = {
+            name : document.getElementById("name").value,
+            phone : document.getElementById("phone").value,
+            email : document.getElementById("email").value,
+        }
+        emailjs.send("service_8vgxzua","template_k3m3zdn",informations).then(alert("Your İnformation has been sent successfully!"))
+        console.log("başarı ile çağırdın")
+    }
