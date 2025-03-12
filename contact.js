99dcc2f59f7fb8d4f4ebf81eef7b64cbf6817234
Muhammed Ali CH <muhammedcihan205@gmail.com>
2025-03-12 17:34:17 +0300

    form yapıldı

--- contact.js
+++ contact.js
@@ -7,6 +7,6 @@
             phone : document.getElementById("phone").value,
             email : document.getElementById("email").value,
         }
-        emailjs.send("service_8vgxzua","template_k3m3zdn",informations).then(alert("Your İnformation has been sent successfully!"))
+        emailjs.send("service_d9qigbk","template_lsr5m4i",informations).then(alert("Your İnformation has been sent successfully!"))
         console.log("başarı ile çağırdın")
     }
